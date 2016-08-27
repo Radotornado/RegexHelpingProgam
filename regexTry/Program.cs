39324147afeb8e1b39a94e40819df9f732661c71
Radotornado <radoslav.mandev99@gmail.com>
2016-08-27 09:32:27 +0300

    Program for helping with arduino code
    Converts frequency and time list to arduino code

---
+++ regexTry/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace regexTry
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // This program was created to ease my work to make a stepper motor to sing "Super Mario" theme song
+            // I got the frequencies and delay from http://wiki.mikrotik.com/wiki/Super_Mario_Theme
+            // The regex for frequency and length
+            Regex rgxFreq = new Regex(@"\:(\w+)\s(\w+)\=(\d+)\s(\w+)\=(\d+)\w+;");
+            // Second regex for the delay
+            Regex rgxDelay = new Regex(@"\:(\w+)\s(\d+)\w+;");
+            List<string> fRow = new List<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "exit")
+            {
+                Match matchBeep = rgxFreq.Match(input);
+                // Checks for match
+                if (matchBeep.Success)
+                {
+                    string freqVal = matchBeep.Groups[3].Value;
+                    string allFirst = "motorStep(" + freqVal + ");";
+                    // Store everything in a list
+                    fRow.Add(allFirst);
+                }
+                Match matchDelay = rgxDelay.Match(input);
+                if (matchDelay.Success)
+                {
+                    string delayVal = matchDelay.Groups[2].Value;
+                    string allSecond = "delay(" + delayVal + ");";
+                    // Store everything in a list
+                    fRow.Add(allSecond);
+                }
+            }
+            foreach (var item in fRow)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
